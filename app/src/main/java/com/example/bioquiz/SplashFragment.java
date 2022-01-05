767a81804e6ea3335ae5aefda47bc60152b9e192
JhonThe17 <83646868+JhonThe17@users.noreply.github.com>
2022-01-05 11:53:53 +0900

    All Layout Changes

---
+++ app/src/main/java/com/example/bioquiz/SplashFragment.java
@@ -0,0 +1,108 @@
+package com.example.bioquiz;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+
+import com.example.bioquiz.helper.SharedPreferenceHelper;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SplashFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SplashFragment extends Fragment {
+    private static int splashtime = 2500;
+    private static final String TAG = "SplashFragment";
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SplashFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SplashFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SplashFragment newInstance(String param1, String param2) {
+        SplashFragment fragment = new SplashFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SharedPreferenceHelper helper = SharedPreferenceHelper.getInstance(requireActivity());
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            NavDirections action;
+            if (helper.getAccessToken().isEmpty()){
+                action = SplashFragmentDirections.actionSplashFragmentToLoginFragment();
+//                action = SplashFragmentDirections.actionSplashFragmentToProjectFragment2();
+            }else{
+                action = SplashFragmentDirections.actionSplashFragmentToProjectFragment2();
+            }
+            Navigation.findNavController(view).navigate(action);
+        }, splashtime);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        ((LoginActivity) getActivity()).getSupportActionBar().hide();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((LoginActivity) getActivity()).getSupportActionBar().hide();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((LoginActivity)getActivity()).getSupportActionBar().show();
+    }
+}
