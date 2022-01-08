615df61bd37a2b3fffefd81a322fd10e3ce80791
Joshua Kouwa Budiman <joshua.kbudiman@gmail.com>
2022-01-08 23:19:41 +0800

    navigasi activity main

--- app/src/main/java/com/example/bioquiz/view/Main/MainActivity.java
+++ app/src/main/java/com/example/bioquiz/view/Main/MainActivity.java
@@ -3,15 +3,29 @@ package com.example.bioquiz.view.Main;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.bioquiz.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavHostFragment navHostFragment;
+    BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNav = findViewById(R.id.bottomNavigationView);
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
+
+        AppBarConfiguration configuration = new AppBarConfiguration.Builder(R.id.homeFragment, R.id.navigation_profile, R.id.navigation_dashboard).build();
+
+        NavigationUI.setupWithNavController(bottomNav, navHostFragment.getNavController());
     }
 }
